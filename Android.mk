f2e16ecb14e9f28f5722e5bca32ac7faee957b25
Matthew Hui <mhui@codeaurora.org>
2011-01-26 02:42:24 +0800

    external/webkit: torodial backing store for scroll acceleration
    Change-Id: If7e478c10a102bae0b8e18a6fd6045357fee6250

--- Android.mk
+++ Android.mk
@@ -64,6 +64,10 @@ ifneq ($(JAVASCRIPT_ENGINE),jsc)
   endif
 endif
 
+ifeq ($(WEBCORE_ACCELERATED_SCROLLING),true)
+        WEBCORE_WEBKITACCEL := true
+endif
+
 BASE_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -173,6 +177,11 @@ LOCAL_C_INCLUDES := $(LOCAL_C_INCLUDES) \
 	$(base_intermediates)/WebCore/html \
 	$(base_intermediates)/WebCore/platform
 
+ifeq ($(WEBCORE_ACCELERATED_SCROLLING),true)
+LOCAL_C_INCLUDES := $(LOCAL_C_INCLUDES) \
+	vendor/qcom/opensource/webkit/BackingStore
+endif
+
 ifeq ($(JAVASCRIPT_ENGINE),v8)
 # Include WTF source file.
 d := JavaScriptCore
@@ -272,6 +281,10 @@ ifeq ($(WEBCORE_INSTRUMENTATION),true)
 LOCAL_CFLAGS += -DANDROID_INSTRUMENT
 endif
 
+ifeq ($(WEBCORE_ACCELERATED_SCROLLING),true)
+LOCAL_CFLAGS += -DENABLE_ACCELERATED_SCROLLING
+endif
+
 # LOCAL_LDLIBS is used in simulator builds only and simulator builds are only
 # valid on Linux
 LOCAL_LDLIBS += -lpthread -ldl
@@ -297,6 +310,10 @@ endif
 
 ifeq ($(WEBCORE_INSTRUMENTATION),true)
 LOCAL_SHARED_LIBRARIES += libhardware_legacy
+endif
+
+ifeq ($(WEBCORE_ACCELERATED_SCROLLING),true)
+LOCAL_SHARED_LIBRARIES += libwebkitaccel
 endif
 
 # We have to use the android version of libdl when we are not on the simulator
